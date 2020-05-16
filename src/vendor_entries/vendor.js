4a4cf989a01d434b0310a43d711fe3ed5d416c89
iamcodergirl <iamcodergirl@gmail.com>
2020-05-16 17:39:12 +0300

    finish projects

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,5 +1,9 @@
 // All files in this file will be included into vendor.js file.
 
 module.exports = [
-  './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/slick-carousel/slick/slick.js',
+    './node_modules/bootstrap/js/dist/util.js',
+    './node_modules/bootstrap/js/dist/collapse.js',
+    './node_modules/bootstrap/js/dist/dropdown.js',
 ];
